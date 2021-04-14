2561bce3e6e0e63467cdc4cf1978f10343b2d2b2
Average Gamer <carlosconsoni7@gmail.com>
2021-04-14 14:16:43 -0400

    Revert "Continued work on Turret Pawn"
    This reverts commit 974c7c11b7d13673417cb9b8b474e2fd0ff5a240.

--- Assets/Scripts/EnemyPawn.cs
+++ Assets/Scripts/EnemyPawn.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EnemyPawn : Pawn
 {
-    Rigidbody rb;
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
